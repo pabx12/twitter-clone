8b52c77bf04ef8f5f7c1d6b9bc27b5f74cb474fd
daouda <nianeddavid@gmail.com>
2022-05-09 07:54:23 +0000

    fix Component return type in _app.tsx

--- .history/pages/_app_20220509074833.tsx
+++ .history/pages/_app_20220509074833.tsx
@@ -7,7 +7,7 @@ import { SessionProvider } from "next-auth/react"
 
 TimeAgo.addDefaultLocale(fr)
 
-function MyApp({ Component, pageProps: { session, ...pageProps } }: any) {
+function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <SessionProvider session={session}>
       <Component {...pageProps} />
